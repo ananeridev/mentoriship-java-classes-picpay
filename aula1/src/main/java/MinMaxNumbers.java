50751e455e4c38e051cc6dab1b27843f1fc26ef3
Ana Beatriz Neri <ananeri32@hotmail.com>
2021-04-20 20:05:13 -0300

    adding examples

---
+++ aula1/src/main/java/MinMaxNumbers.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class MinMaxNumbers {
+    /*
+     * "Ler 3 números inteiros da entrada, imprimir o menor e o maior"
+     */
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n1, n2, n3;
+
+            System.out.print("Entre com o primeiro inteiro: ");
+            n1 = sc.nextInt();
+            System.out.print("Entre com o segundo inteiro: ");
+            n2 = sc.nextInt();
+            System.out.print("Entre com o terceiro inteiro: ");
+            n3 = sc.nextInt();
+            if (n1 > n2) {
+                if (n1 > n3) {
+                    if (n2 < n3) {
+                        System.out.println("O menor numero eh: " + n2);
+                    } else {
+                        System.out.println("O menor numero eh: " + n3);
+                    }
+                    System.out.println("O maior numero eh: " + n1);
+                } else {
+                    if (n1 < n2) {
+                        System.out.println("O menor numero eh: " + n1);
+                    } else {
+                        System.out.println("O menor numero eh: " + n2);
+                    }
+                    System.out.println("O maior numero eh: " + n3);
+                }
+            } else {
+                if (n2 > n3) {
+                    if (n1 < n3) {
+                        System.out.println("O menor numero eh: " + n1);
+                    } else {
+                        System.out.println("O menor numero eh: " + n3);
+                    }
+                    System.out.println("O maior numero eh: " + n2);
+                } else {
+                    if (n1 < n2) {
+                        System.out.println("O menor numero eh: " + n1);
+                    } else {
+                        System.out.println("O menor numero eh: " + n2);
+                    }
+                    System.out.println("O maior numero eh: " + n3);
+                }
+            }
+        }
+    }
