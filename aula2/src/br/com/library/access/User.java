c9af3523792060b3bb21e514e3250f9b49521c29
Ana Beatriz Neri <ananeri32@hotmail.com>
2021-05-01 12:26:12 -0300

    adicionando metodo user

--- aula2/src/br/com/library/access/User.java
+++ aula2/src/br/com/library/access/User.java
@@ -1,4 +1,59 @@
 package br.com.library.access;
 
 public class User {
+
+    // atributos com suas devidas caracteristicas
+    private Long codigo;
+    private String nomeCompleto;
+    private String dataNascimento;
+    private String endereco;
+
+    // construtores
+    public User(Long codigo, String nomeCompleto, String dataNascimento, String endereco) {
+        this.codigo = codigo;
+        this.nomeCompleto = nomeCompleto;
+        this.dataNascimento = dataNascimento;
+        this.endereco = endereco;
+    }
+
+    // encapsulamento dos objetos
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void userPrint() {
+        System.out.println("Codigo" +this.getCodigo());
+        System.out.println("Nome Compelto" +this.getNomeCompleto());
+        System.out.println("Nascimento" +this.getDataNascimento());
+        System.out.println("Endereco" +this.getEndereco());
+    }
+
 }
